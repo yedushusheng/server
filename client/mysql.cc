371e17df9e25b288dcb4db962d167df329d36ce3
Monty <monty@mariadb.org>
2020-08-22 02:08:59 +0300

    Change CHARSET_INFO character set and collaction names to LEX_CSTRING
    This change removed 68 strlen() calls from the code.
    
    The following renames was to ensure we don't use the old names
    when merging code from earlier releases, as using the new variables
    for print function could result in crashes:
    - charset->csname renamed to charset->cs_name
    - charset->name renamed to charset->coll_name
    
    Almost everything where mechanical changes except:
    - Changed to use the new Protocol::store(LEX_CSTRING..) when possible
    - Changed to use field->store(LEX_CSTRING*, CHARSET_INFO*) when possible
    - Changed to use String->append(LEX_CSTRING&) when possible
    
    Other things:
    - There where compiler issues with ensuring that all character set names
      points to the same string: gcc doesn't allow one to use integer constants
      when defining global structures (constant char * pointers works fine).
      To get around this, I declared defines for each character set name
      length.

--- client/mysql.cc
+++ client/mysql.cc
@@ -3182,8 +3182,8 @@ com_charset(String *buffer __attribute__((unused)), char *line)
   if (new_cs)
   {
     charset_info= new_cs;
-    mysql_set_character_set(&mysql, charset_info->csname);
-    default_charset= (char *)charset_info->csname;
+    mysql_set_character_set(&mysql, charset_info->cs_name.str);
+    default_charset= (char *)charset_info->cs_name.str;
     put_info("Charset changed", INFO_INFO);
   }
   else put_info("Charset is not found", INFO_INFO);
@@ -4672,6 +4672,8 @@ static int
 sql_real_connect(char *host,char *database,char *user,char *password,
 		 uint silent)
 {
+  const char *charset_name;
+
   if (connected)
   {
     connected= 0;
@@ -4719,13 +4721,17 @@ sql_real_connect(char *host,char *database,char *user,char *password,
     return -1;					// Retryable
   }
 
-  if (!(charset_info= get_charset_by_name(mysql.charset->name, MYF(0))))
+  charset_name= IF_EMBEDDED(mysql.charset->coll_name.str,
+                            mysql.charset->name);
+  charset_info= get_charset_by_name(charset_name, MYF(0));
+  if (!charset_info)
   {
-    put_info("Unknown default character set", INFO_ERROR);
+    char buff[128];
+    my_snprintf(buff, sizeof(buff)-1,
+                "Unknown default character set %s", charset_name);
+    put_info(buff, INFO_ERROR);
     return 1;
   }
-
-  
   connected=1;
 #ifndef EMBEDDED_LIBRARY
   mysql_options(&mysql, MYSQL_OPT_RECONNECT, &debug_info_flag);
@@ -4864,8 +4870,9 @@ com_status(String *buffer __attribute__((unused)),
   else
   {
     /* Probably pre-4.1 server */
-    tee_fprintf(stdout, "Client characterset:\t%s\n", charset_info->csname);
-    tee_fprintf(stdout, "Server characterset:\t%s\n", mysql.charset->csname);
+    tee_fprintf(stdout, "Client characterset:\t%s\n", charset_info->cs_name.str);
+    tee_fprintf(stdout, "Server characterset:\t%s\n",
+                mysql_character_set_name(&mysql));
   }
 
 #ifndef EMBEDDED_LIBRARY
